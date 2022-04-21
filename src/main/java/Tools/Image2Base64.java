6afcf9432c213b486185684989939a71bcbe74b4
testowy <zarzycki.mic@gmail.com>
2022-04-21 14:56:47 +0200

    drobna poprawka wyjasniajaca

--- src/main/java/Tools/Image2Base64.java
+++ src/main/java/Tools/Image2Base64.java
@@ -7,10 +7,9 @@ import java.io.IOException;
 import java.util.Base64;
 
 //@ https://www.baeldung.com/java-base64-image-string
-public class ImageConverter {
+public class Image2Base64 {
 
-    static public String imageConversion() throws IOException {
-        String filePath= "C:\\Users\\zarzy\\Desktop\\praca\\ZadanieProjektowe\\projektRekrutacyjny\\src\\main\\resources\\Kwiaty.jpg";
+    static public String imageConversion(String filePath) throws IOException {
         byte[] fileContent = FileUtils.readFileToByteArray(new File(filePath));
         String encodedString = Base64.getEncoder().encodeToString(fileContent);
         return encodedString;
